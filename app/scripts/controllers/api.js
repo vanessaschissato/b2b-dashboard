59d10263cc4aca9a15062190a86da94a1e4fa94c
Vanessa Schissato <vanessaschissato@gmail.com>
2015-12-07 19:49:50 -0200

    Dashboard transations

--- app/scripts/controllers/api.js
+++ app/scripts/controllers/api.js
@@ -1,10 +1,32 @@
 'use strict';
 
 angular.module('dashboardApp')
-.controller('ApiCtrl', ['$scope', 'ApiService', function($scope, ApiService) {
-	
-  $scope.status = ApiService.getStatus()
+.controller('ApiCtrl', ['$scope', '$interval', '$location', 'ApiService', function($scope, $interval, $location, ApiService) {
 
-  $scope.lines = Math.ceil($scope.status.apis.length / 3);
+	var fixedEnvironment = $location.search().environment;
+
+	// TODO: REFACTOR THIS!
+	if (fixedEnvironment && fixedEnvironment.length > 0) {
+		$scope.status = ApiService.getStatus(fixedEnvironment);
+  		$scope.lines = Math.ceil($scope.status.apis.length / 3);
+  		return;
+	}
+
+  	var environments = ApiService.getEnvironments();
+  	var index = 0;
+  	$interval(function() {
+
+  		$scope.status = {}
+  		var environment = environments[index];
+
+  		$scope.status = ApiService.getStatus(environment.code);
+  		$scope.lines = Math.ceil($scope.status.apis.length / 3);
+
+  		index = (index >= environments.length - 1) ? 0 : ++index;
+  		
+  	}, 5000)
+  	
+
+  	
 
 }]);
--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -4,9 +4,10 @@ angular.module('dashboardApp')
 .service('ApiService', function() {
 
 	 // Mocked
-	this.getStatus = function() {
-		return {
-	  	"environments": 
+	this.getEnvironments = function() {
+		
+	  	var response = {
+	  		"environments": 
 	  		[
 	  			{
 	    			"code": "ar",
@@ -22,127 +23,77 @@ angular.module('dashboardApp')
 	    		}
 			]
 		}
+
+		return response.environments;
 	}
 
     // Mocked
-	this.getStatus = function() {
-		return {
-	  	"geral": {
-	    	"environment": {
-	    		"code": "ar",
-	    		"name": "Argentina"
-	    	},
-	    	"error": true
-	  	},
-	  	"apis": 
-	  		[
-		      	{
-		      		"name": "POST /authentication",
-		      		"time": 2200,
-		      		"code": "ERROR"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "TIMEOUT"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "ERROR"
-		      	},
-		      	{
-		      		"name": "POST /authentication",
-		      		"time": 2200,
-		      		"code": "ERROR"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "TIMEOUT"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "ERROR"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "ERROR"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "ERROR"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		       		"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "SUCCESS"
-		      	},
-		      	{
-		        	"name": "GET /order",
-		        	"time": 300,
-		        	"code": "ERROR"
-		        }
-	  		]
-	  	}
+	this.getStatus = function(code) {
+		var response = {
+		  	"geral": {
+		    	"environment": {
+		    		"code": code,
+		    		"name": "Argentina"
+		    	},
+		    	"error": true
+		  	},
+		  	"apis": 
+		  		[
+			      	{
+			      		"name": "POST /authentication",
+			      		"time": 2200,
+			      		"code": "ERROR"
+			      	},
+			      	{
+			        	"name": "GET /order",
+			        	"time": 300,
+			        	"code": "SUCCESS"
+			      	},
+			      	{
+			        	"name": "GET /order",
+			        	"time": 300,
+			        	"code": "TIMEOUT"
+			      	},
+			      	{
+			        	"name": "GET /order",
+			        	"time": 300,
+			        	"code": "SUCCESS"
+			      	},
+			      	{
+			        	"name": "GET /order",
+			        	"time": 300,
+			        	"code": "SUCCESS"
+			      	},
+			      	{
+			        	"name": "GET /order",
+			        	"time": 300,
+			        	"code": "ERROR"
+			      	},
+			      	{
+			      		"name": "POST /authentication",
+			      		"time": 2200,
+			      		"code": "ERROR"
+			      	},
+			      	{
+			        	"name": "GET /order",
+			        	"time": 300,
+			        	"code": "SUCCESS"
+			      	},
+			      	{
+			        	"name": "GET /order",
+			        	"time": 300,
+			        	"code": "TIMEOUT"
+			      	},
+			      	{
+			        	"name": "GET /order",
+			        	"time": 300,
+			        	"code": "SUCCESS"
+			      	}
+		  		]
+		  	}
+
+
+		return response;
 	}
 
 });
