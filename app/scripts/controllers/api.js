276d8cbf020370f69ccc021b69e4a439548b1132
Vanessa Schissato <vanessaschissato@gmail.com>
2015-12-07 17:27:11 -0200

    Automatica resizing

--- app/scripts/controllers/api.js
+++ app/scripts/controllers/api.js
@@ -9,6 +9,123 @@ angular.module('dashboardApp.api', ['ngRoute'])
   });
 }])
 
-.controller('ApiCtrl', [function() {
+.controller('ApiCtrl', ['$scope', function($scope) {
+
+  // Mock
+  $scope.status = {
+  	"geral": {
+    	"environment": "ar",
+    	"error": true
+  	},
+  	"apis": [
+      {
+      	"name": "POST /authentication",
+      	"time": 2200,
+      	"code": "ERROR"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "TIMEOUT"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "ERROR"
+      },
+      {
+      	"name": "POST /authentication",
+      	"time": 2200,
+      	"code": "ERROR"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "TIMEOUT"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "ERROR"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "ERROR"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "ERROR"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "SUCCESS"
+      },
+      {
+        "name": "GET /order",
+        "time": 300,
+        "code": "ERROR"
+      }
+  	]
+  }
+
+  $scope.lines = Math.ceil($scope.status.apis.length / 3);
 
 }]);
